ae16dddbcaba11be8d8feb33c16f9ad9a46fffc0
Michel Kong <minkya_u@daum.net>
2019-10-10 16:41:27 -0700

    starting with the questions

---
+++ Assets/javascript.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which month has 28 days?",
+        choices: ["January", "Febuary", "March", "All months"],
+        answer: 'All months'
+    },
+    {
+        title: "If you were running a race and you passed the person in 2nd place, what place would you be in now?",
+        choices: ['first','second','third','last'],
+        answer: 'second'
+    },
+    {
+        title: "What are the next two letters in this series? JFMAMJJASO",
+        choices: ['JM', 'AJ', "BC", "ND"],
+        answer: 'ND'
+    },
+    {
+        title: "How many letters are in the alphabet?",
+        choices: ['6', '7', "8", "9"],
+        answer: '8'
+    },
+    {
+        title: "How many letters are in the alphabet?",
+        choices: ['6', '25', "8", "9"],
+        answer: '8'
+    },
+  ];
