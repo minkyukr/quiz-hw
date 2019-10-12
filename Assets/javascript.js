c01bf40288401c13d32484c9374bdb5ec4c131e4
minkyukr <minkya_u@daum.net>
2019-10-11 23:20:38 -0700

    after clicking the start button, it disappears but the questions are not there

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,35 +1,51 @@
 var questions = [
     {
-        title: "Which month has 28 days?",
-        choices: ["January", "Febuary", "March", "All months"],
-        answer: 'All months'
+        question: "Which month has 28 days?",
+        choice1: "January",
+        choice2: "Febuary",
+        choice3: "March", 
+        choice4: "All months",
+        answer: '4'
     },
     {
-        title: "If you were running a race and you passed the person in 2nd place, what place would you be in now?",
-        choices: ['first','second','third','last'],
-        answer: 'second'
+        question: "If you were running a race and you passed the person in 2nd place, what place would you be in now?",
+        choice1: 'first',
+        choice2: 'second',
+        choice3: 'third',
+        choice4: 'last',
+        answer: '2'
     },
     {
-        title: "What are the next two letters in this series? JFMAMJJASO",
-        choices: ['JM', 'AJ', "BC", "ND"],
-        answer: 'ND'
+        question: "What are the next two letters in this series? JFMAMJJASO",
+        choice1: 'JM',
+        choice2: 'AJ',
+        choice3: 'BC',
+        choice4: 'ND',
+        answer: '4'
     },
     {
-        title: "How many letters are in the alphabet?",
-        choices: ['6', '7', "8", "9"],
-        answer: '8'
+        question: "How many letters are in the alphabet?",
+        choice1: '6',
+        choice2: '7',
+        choice3: '8',
+        choice4: '9',
+        answer: '3'
     },
     {
-        title: "June's dad has four daughters, January is the first daughter, February is the second daughter , March is the third daughter, What is the name of the fourth child",
-        choices: ['April', 'May', "June", "July"],
-        answer: 'June'
+        question: "June's dad has four daughters, January is the first daughter, February is the second daughter , March is the third daughter, What is the name of the fourth child",
+        choice1: 'April',
+        choice2: 'May',
+        choice3: 'June',
+        choice4: 'July',
+        correct: '3'
     },
   ];
 
 var timeEl = document.querySelector(".col-md-1");
 var timerEl = document.getElementById("#timer");
-var startButton = document.querySelector('.start');
-var secondsLeft = 50;
+var startButton = document.querySelector('.quizBox');
+var questionBox = document.querySelector('.questionBox')
+var secondsLeft = 51;
 
 var choicesEl = document.getElementById('#choices');
 var choiceEl = document.getElementById('.choice');
@@ -40,7 +56,7 @@ var choiceC = document.getElementById('#C');
 var choiceD = document.getElementById('#D');
 
 startButton.addEventListener('click', function (event) {
-    event.preventDefault();
+    startButton.style.display = 'none'; 
 
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -48,8 +64,60 @@ startButton.addEventListener('click', function (event) {
 
         if(secondsLeft === 0) {
         clearInterval(timerInterval);
-        alert('Sorry. Please try again.');
+        alert('Sorry. Refresh the page and please try again.');
             }
         }, 1000);
 
+    questionBox.style.display = 'block';
+
+    
+
     })
+  
+//     lastQuestionIndex = questions.length-1;
+//     var runningQuestionIndex = 0;
+
+//         function renderQuestion(event) {
+//             let q = question[runningQuestionIndex];
+//             question.innerHTML = '<p>' + q.question+ '</p>';
+//             choiceA.innerHTML = q.choiceA;
+//             choiceB.innerHTML = q.choiceB;
+//             choiceC.innerHTML = q.choiceC;
+//             choiceD.innerHTML = q.choiceD;
+//             runningQuestionIndex = 0;}
+//             renderQuestion()
+
+//             runningQuestionIndex++;
+//             renderQuestion();
+
+//     score = 0;
+//     function checkAnswer(answer) {
+//         if(questions[runningQuestionIndex].correct == answer){
+//             alert('Good guess!')
+//         } else{
+//             answerIsWrong();
+//             answer = answer - 10;
+//         } if(runningQuestionIndex < lastQuestionIndex){
+//             runningQuestionIndex++;
+//             questionRender();
+//         } else{
+//             scoreRender();
+//         }
+//     }
+//     // })
+
+// startButton.addEventListener('click', startQuiz);
+// function startQuiz() {
+//     startButton.style.display = 'none'; 
+//     var timerInterval = setInterval(function() {
+//         secondsLeft--;
+//         timeEl.textContent = secondsLeft
+
+//         if(secondsLeft === 0) {
+//         clearInterval(timerInterval);
+//         alert('Sorry. Refresh the page and please try again.');
+//             }
+//         }, 1000);   
+//         renderQuestion();
+//         quiz.style.display = 'block';
+// }
